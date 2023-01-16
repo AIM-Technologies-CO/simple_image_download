df9a421a416df2b8559fe8d5815ab7581d02ea8e
rashwan <ahmed.t.rashwan@gmail.com>
2023-01-16 18:27:21 +0000

    fix version of python magic

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 
-with open('README.md', encoding='utf-8') as readme_file:
+with open("README.md", encoding="utf-8") as readme_file:
     README = readme_file.read()
 
 setup_args = dict(
-    name='simple_image_download',
-    version='0.4',
-    description='Downloads raw page from google images and searches for images that can be downloaded',
+    name="simple_image_download",
+    version="0.4",
+    description="Downloads raw page from google images and searches for images that can be downloaded",
     long_description=README,
-	long_description_content_type='text/markdown',
-    license='MIT',
+    long_description_content_type="text/markdown",
+    license="MIT",
     packages=find_packages(),
-    author='Jakub Dobies',
-    author_email='kuba.dobies@onet.pl',
-    keywords=['google images', 'image downloader'],
-    url='https://github.com/RiddlerQ/simple_image_download',
-    download_url='https://pypi.org/project/simple_image_download/'
+    author="Jakub Dobies",
+    author_email="kuba.dobies@onet.pl",
+    keywords=["google images", "image downloader"],
+    url="https://github.com/RiddlerQ/simple_image_download",
+    download_url="https://pypi.org/project/simple_image_download/",
 )
 
-install_requires = ['requests','python-magic-bin==0.4.14','progressbar']
+install_requires = ["requests", "python-magic==0.4.15", "progressbar"]
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     setup(**setup_args, install_requires=install_requires)
